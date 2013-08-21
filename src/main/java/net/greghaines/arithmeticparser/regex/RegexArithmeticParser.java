8ade4b1312564f318cfd59f81accceb8ee404418
Greg Haines <gresun@gmail.com>
2013-08-21 14:01:38 -0400

    Regex now works

--- src/main/java/net/greghaines/arithmeticparser/regex/RegexArithmeticParser.java
+++ src/main/java/net/greghaines/arithmeticparser/regex/RegexArithmeticParser.java
@@ -8,8 +8,9 @@ import net.greghaines.arithmeticparser.ArithmeticParser;
 public class RegexArithmeticParser implements ArithmeticParser {
     
     private static final Pattern parenPattern = Pattern.compile("\\(([^\\)\\(]+)\\)");
-    private static final Pattern multiDividePattern = Pattern.compile("([-]?[0-9]+(?:\\.[0-9]+)?)\\s*([\\*\\/])\\s*([-]?[0-9]+(?:\\.[0-9]+)?)");
-    private static final Pattern addSubPattern = Pattern.compile("([-]?[0-9]+(?:\\.[0-9]+)?)\\s*([\\+\\-])\\s*([-]?[0-9]+(?:\\.[0-9]+)?)");
+    private static final String numericPatternStr = "([-]?(?:[0-9]+(?:\\.[0-9]+)?|Infinity|NaN))";
+    private static final Pattern multiDividePattern = Pattern.compile(numericPatternStr + "\\s*([\\*\\/])\\s*" + numericPatternStr);
+    private static final Pattern addSubPattern = Pattern.compile(numericPatternStr + "\\s*([\\+\\-])\\s*" + numericPatternStr);
     private static final Pattern[] opPatterns = { multiDividePattern, addSubPattern };
 
     /**
@@ -17,7 +18,6 @@ public class RegexArithmeticParser implements ArithmeticParser {
      */
     @Override
     public double evaluate(final String expression) {
-//        System.out.println("evaluate: " + expression);
         String text = expression;
         final Matcher p = parenPattern.matcher(text);
         while (p.reset(text).find()) {
@@ -25,40 +25,28 @@ public class RegexArithmeticParser implements ArithmeticParser {
         }
         for (final Pattern opPattern : opPatterns) {
             p.usePattern(opPattern);
-            // FIXME: Handle Infinity, -Infinity and NaN
             while (p.reset(text).find()) {
-//                System.out.println("PRE text: " + text);
                 final String arg1 = p.group(1);
                 final String operator = p.group(2);
                 final String arg2 = p.group(3);
-//                System.out.println("arg1: " + arg1);
-//                System.out.println("operator: " + operator);
-//                System.out.println("arg2: " + arg2);
                 final double result;
                 switch (operator) {
-                case "*": {
+                case "*":
                     result = evaluate(arg1) * evaluate(arg2);
                     break;
-                }
-                case "/": {
+                case "/":
                     result = evaluate(arg1) / evaluate(arg2);
                     break;
-                }
-                case "+": {
+                case "+":
                     result = evaluate(arg1) + evaluate(arg2);
                     break;
-                }
-                case "-": {
+                case "-":
                     result = evaluate(arg1) - evaluate(arg2);
                     break;
-                }
-                default: {
+                default:
                     throw new IllegalArgumentException("Unknown operator: " + operator);
                 }
-                }
-//                System.out.println("result: " + result);
                 text = text.substring(0, p.start()) + result + text.substring(p.end());
-//                System.out.println("POST text: " + text);
             }
         }
         return Double.parseDouble(text);
--- src/test/java/net/greghaines/arithmeticparser/regex/TestRegexArithmeticParser.java
+++ src/test/java/net/greghaines/arithmeticparser/regex/TestRegexArithmeticParser.java
@@ -1,15 +1,12 @@
 package net.greghaines.arithmeticparser.regex;
 
 import net.greghaines.arithmeticparser.TestArithmeticParser;
-import net.greghaines.arithmeticparser.regex.RegexArithmeticParser;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestRegexArithmeticParser extends TestArithmeticParser {
 
     @Test
-    @Ignore // Ignore while this doesn't work
     public void testEvaluate() {
         doTest(new RegexArithmeticParser());
     }
