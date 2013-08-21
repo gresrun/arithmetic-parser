1adb215ac1992c1b0b2a092eef28b845ed9c3bc5
Greg Haines <gresun@gmail.com>
2013-08-21 10:21:27 -0400

    Handle negative numbers correctly in regex version

--- src/main/java/net/greghaines/arithmeticparser/regexor/RegexArithmeticParser.java
+++ src/main/java/net/greghaines/arithmeticparser/regexor/RegexArithmeticParser.java
@@ -8,8 +8,8 @@ import net.greghaines.arithmeticparser.ArithmeticParser;
 public class RegexArithmeticParser implements ArithmeticParser {
     
     private static final Pattern parenPattern = Pattern.compile("\\(([^\\)\\(]+)\\)");
-    private static final Pattern multiDividePattern = Pattern.compile("([0-9]+(\\.[0-9]+)?)\\s*([\\*\\/])\\s*([0-9]+(\\.[0-9]+)?)");
-    private static final Pattern addSubPattern = Pattern.compile("([0-9]+(\\.[0-9]+)?)\\s*([\\+\\-])\\s*([0-9]+(\\.[0-9]+)?)");
+    private static final Pattern multiDividePattern = Pattern.compile("([-]?[0-9]+(?:\\.[0-9]+)?)\\s*([\\*\\/])\\s*([-]?[0-9]+(?:\\.[0-9]+)?)");
+    private static final Pattern addSubPattern = Pattern.compile("([-]?[0-9]+(?:\\.[0-9]+)?)\\s*([\\+\\-])\\s*([-]?[0-9]+(?:\\.[0-9]+)?)");
     private static final Pattern[] opPatterns = { multiDividePattern, addSubPattern };
 
     /**
@@ -17,6 +17,7 @@ public class RegexArithmeticParser implements ArithmeticParser {
      */
     @Override
     public double evaluate(final String expression) {
+//        System.out.println("evaluate: " + expression);
         String text = expression;
         final Matcher p = parenPattern.matcher(text);
         while (p.reset(text).find()) {
@@ -25,10 +26,38 @@ public class RegexArithmeticParser implements ArithmeticParser {
         for (final Pattern opPattern : opPatterns) {
             p.usePattern(opPattern);
             while (p.reset(text).find()) {
-                text = text.substring(0, p.start())
-                        + (p.group(3).equals("*") ? (evaluate(p.group(1)) * evaluate(p.group(4))) : (p.group(3).equals("/") ? (evaluate(p.group(4)) == 0 ? Double.NaN
-                                : (evaluate(p.group(1)) / evaluate(p.group(4)))) : (p.group(3).equals("+") ? (evaluate(p.group(1)) + evaluate(p.group(4))) : (evaluate(p.group(1)) - evaluate(p
-                                .group(4)))))) + text.substring(p.end());
+//                System.out.println("PRE text: " + text);
+                final String arg1 = p.group(1);
+                final String operator = p.group(2);
+                final String arg2 = p.group(3);
+//                System.out.println("arg1: " + arg1);
+//                System.out.println("operator: " + operator);
+//                System.out.println("arg2: " + arg2);
+                final double result;
+                switch (operator) {
+                case "*": {
+                    result = evaluate(arg1) * evaluate(arg2);
+                    break;
+                }
+                case "/": {
+                    result = evaluate(arg1) / evaluate(arg2);
+                    break;
+                }
+                case "+": {
+                    result = evaluate(arg1) + evaluate(arg2);
+                    break;
+                }
+                case "-": {
+                    result = evaluate(arg1) - evaluate(arg2);
+                    break;
+                }
+                default: {
+                    throw new IllegalArgumentException("Unknown operator: " + operator);
+                }
+                }
+//                System.out.println("result: " + result);
+                text = text.substring(0, p.start()) + result + text.substring(p.end());
+//                System.out.println("POST text: " + text);
             }
         }
         return Double.parseDouble(text);
